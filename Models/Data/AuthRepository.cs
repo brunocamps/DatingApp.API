4d68f433cc8e085ff86a65d9d375bc64e1a7a81b
Bruno Campos <30634714+brunocamps@users.noreply.github.com>
2019-11-08 15:52:26 -0300

    Login Repository Done
    Next -> inject login repository into classes

--- Models/Data/AuthRepository.cs
+++ Models/Data/AuthRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Models.Data
 {
@@ -11,12 +12,36 @@ namespace DatingApp.API.Models.Data
         {
             _context = context;
         }
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            //username to identify the user in our database
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null)
+                return null;
+            
+            if(!VerifyPasswordHash(password, user.PasswordHash,user.PasswordSalt))
+                return null; //if the password doesn't match. Boolean function. 
+
+            return user;
         }
 
-        public async Task<User> Register(User user, string password)
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                //passwordSalt = hmac.Key;
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                //this is going to compute the hash from our password
+                //computedHash must be the same as passwordHash above
+                for(int i = 0; i < computedHash.Length; i++){
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            } 
+            return true; //because we know the passwords match!
+        }
+
+        public async Task<User> Register(User user, string password) //returns a task of type user
         {
             //throw new System.NotImplementedException();
             //password hash and password salt
@@ -48,9 +73,13 @@ namespace DatingApp.API.Models.Data
 
 
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            //compare the username against any user in the database
+            if(await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+
+            return false;
         }
     }
 }
