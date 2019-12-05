d511b01abd487acba2993a5d0d6fd8100ea92d93
Bruno Campos <30634714+brunocamps@users.noreply.github.com>
2019-12-04 21:45:43 -0300

    End of Section 4
    Summary:
    
    - Angular services (communicate with API)
    - Conditionaly dislaying elements in the DOM
    - Input properties
    - Output properties
    - Client login and register

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -8,25 +8,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
 
   constructor(private http: HttpClient) { } // inject the HTTP client
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = true;
   }
 
+  /*
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response; // this is gonna contain the object with the value inside
     }, error => {
       console.log(error);
     } );
-  }
+  } */
 
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
